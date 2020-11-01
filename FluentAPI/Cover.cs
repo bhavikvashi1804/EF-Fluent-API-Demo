e0a4aa46b8edcbd74b76ec6d4a9dbaa40e91d9a0
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-01 15:46:32 +0530

    Create a new class Course

---
+++ FluentAPI/Cover.cs
@@ -0,0 +1,17 @@
+﻿using DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+    class Cover
+    {
+        public int Id { get; set; }
+
+        //Annotation Property
+        public Course Course { get; set; }
+    }
+}
