c180fdb932b16dcbacd4c87ee7eb3d05a9b7dc66
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-11-01 16:02:29 +0530

    solve the error for one to one relationship

--- FluentAPI/PlutoContext.cs
+++ FluentAPI/PlutoContext.cs
@@ -44,6 +44,11 @@ namespace DataAnnotations
                 .WithMany(t => t.Courses)
                 .Map(m => m.ToTable("CourseTags"));
 
+            modelBuilder
+                .Entity<Course>()
+                .HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
 
             base.OnModelCreating(modelBuilder);
         }
